5658b3334b8663cad80cd4c2d43542a70c016309
gianni <terrocraft3223@gmail.com>
2025-10-11 22:31:51 +0200

    Add new modules.

--- transformers/item.js
+++ transformers/item.js
@@ -7,24 +7,32 @@ module.exports.transform = (item) => {
     [ItemKey]: {
 
       material: (item.modules?.baseMaterial || `PAPER`).toLowerCase(),
-      settings: {
-        //I need a lot of custom modules later.
-        //https://xiao-momi.github.io/craft-engine-wiki/configuration/item/settings
-        unbreakable: item.modules?.unbreakable,
-        enchanenchantable: item.modules?.disableEnchanting === true ? false : true,
 
-        food: {
-          nutrition: item.modules?.nutrition,
-          saturation: item.modules?.saturation,
-        }
-          
-      },
 
       data: {
         "item-name": item.name,
+        "custom-model-data": item.modules?.customModelData,
         lore: item.modules?.lore,
+        unbreakable: item.modules?.unbreakable,
+
+        food: {
+            nutrition: item.modules?.nutrition,
+            saturation: item.modules?.saturation,
+        },
+
+        components: {
+            "minecraft:max_damage": item.modules?.damage,
+            "minecraft:enchantment_glint_override": item.modules?.enchantmentGlintOverride,
+            "minecraft:max_stack_size": item.modules?.maxStackSize,   
+        }
       },
 
+      settings: {
+        //I need a lot of custom modules later.
+        //https://xiao-momi.github.io/craft-engine-wiki/configuration/item/settings
+        enchanenchantable: item.modules?.disableEnchanting === true ? false : true,
+          
+      },
 
     },
   };
