ffec24daad848d72875e40cc2349059ee72260d7
gianni <terrocraft3223@gmail.com>
2025-10-11 21:56:51 +0200

    format

--- transformers/item.js
+++ transformers/item.js
@@ -1,32 +1,31 @@
 module.exports.transform = (item) => {
-    {
-      const ItemKey = `${item.namespace}:${item.id}`;
+
+    const ItemKey = `${item.namespace}:${item.id}`;
       
-      return {
+    return {
 
-        [ItemKey]: {
+    [ItemKey]: {
 
-          material: (item.modules?.baseMaterial || `PAPER`).toLowerCase(),
-          settings: {
-            //I need a lot of custom modules later.
-            //https://xiao-momi.github.io/craft-engine-wiki/configuration/item/settings
-            unbreakable: item.modules?.unbreakable,
-            enchanenchantable: item.modules?.disableEnchanting === true ? false : true,
+      material: (item.modules?.baseMaterial || `PAPER`).toLowerCase(),
+      settings: {
+        //I need a lot of custom modules later.
+        //https://xiao-momi.github.io/craft-engine-wiki/configuration/item/settings
+        unbreakable: item.modules?.unbreakable,
+        enchanenchantable: item.modules?.disableEnchanting === true ? false : true,
 
-            food: {
-              nutrition: item.modules?.nutrition,
-              saturation: item.modules?.saturation,
-            }
+        food: {
+          nutrition: item.modules?.nutrition,
+          saturation: item.modules?.saturation,
+        }
           
-          },
-
-          data: {
-            "item-name": item.name,
-            lore: item.modules?.lore,
-          },
-
-
       },
-      };
-    }
+
+      data: {
+        "item-name": item.name,
+        lore: item.modules?.lore,
+      },
+
+
+    },
+  };
 }
