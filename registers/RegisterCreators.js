8989c60bd5a1060636ac20bcbf74a90702d33387
CoolGo1425 <93942561+CoolGo1425@users.noreply.github.com>
2025-12-14 21:51:15 +0200

    commitnpush

--- registers/RegisterCreators.js
+++ registers/RegisterCreators.js
@@ -1,20 +1,11 @@
 ﻿module.exports = (nm) => {
   // Enable built-in creators for CraftEngine projects
   nm.postCreatorTypeCompatibilities({
-<<<<<<< HEAD
-    'create_item': ['craftengine'],
-    'create_weapon': ['craftengine'],
-    'create_tool': ['craftengine'],
-    'create_armor': ['craftengine'],
-    'create_food': ['craftengine'],
-    'create_block': ['craftengine'],
-=======
     'create_weapon': ['craftengine'],
     'create_block': ['craftengine'],
     'create_tool': ['craftengine'],
     'create_food': ['craftengine'],
     'create_item': ['craftengine'],
->>>>>>> 63ce9621209cb61262383f9443d1024bdf884edd
     'create_furniture': ['craftengine']
   }, { merge: true });
 };
