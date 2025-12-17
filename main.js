edebd4d79db969a99774a02f321fc51113e706b0
CoolGo1425 <93942561+CoolGo1425@users.noreply.github.com>
2025-12-18 01:18:51 +0200

    license update

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+/*
+    CraftEngine Support Expansion for Nexo Maker - Adding Support for the CraftEngine Minecraft Plugin to the Nexo Maker Application
+    Copyright (C) 2025  Ido Ariel aka dodi2020
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 module.exports.init = async () => {
   const nm = api.nexomaker;
   const itemTransformer = require("./transformers/transformer.js");
